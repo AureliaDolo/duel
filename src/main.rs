1d1a5a5db65c0309afb994db27878a29d4a25e0d
Aurelia <aurelia.dolo@gmail.com>
2022-11-19 14:44:55 +0100

    Player manegement.
    Based on https://github.com/not-fl3/macroquad/blob/master/examples/platformer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use macroquad::prelude::*;
+use macroquad_platformer::{Actor, Tile, World};
 
-type Stage = Vec<[Block; 16]>;
-enum Block {
-    Air,
-    Solid,
-    //Liquid,
-    Debug,
+// Source d'inspiration https://github.com/not-fl3/macroquad/blob/master/examples/platformer.rs
+
+type Stage = Vec<[Tile; 16]>;
+
+enum DuelError {
+    PlayerInTheGround,
 }
 
 const BLOCK_SIZE: f32 = 16.;
-const PLAYER_WIDTH: f32 = 1.;
+const DELTA: f32 = 1.;
+const PLAYER_WIDTH: f32 = 10.;
 const PLAYER_HEIGHT: f32 = 15.;
 
 // TODO player
@@ -17,43 +19,39 @@ const PLAYER_HEIGHT: f32 = 15.;
 // TODO exit
 
 fn init_stage() -> Stage {
-    use Block::*;
+    use Tile::*;
     vec![
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Solid, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Solid, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
-            Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air, Air,
-        ],
-        [
-            Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
-            Solid, Solid, Solid, Solid,
-        ],
-        [
-            Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
-            Solid, Solid, Solid, Solid,
-        ],
-        [
-            Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
-            Solid, Solid, Solid, Solid,
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            Empty, Empty, Empty, Empty,
         ],
         [
             Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
@@ -69,7 +67,19 @@ fn init_stage() -> Stage {
         ],
         [
             Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
-            Solid, Solid, Air, Solid,
+            Solid, Solid, Solid, Solid,
+        ],
+        [
+            Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
+            Solid, Solid, Solid, Solid,
+        ],
+        [
+            Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
+            Solid, Solid, Solid, Solid,
+        ],
+        [
+            Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
+            Solid, Solid, Empty, Solid,
         ],
         [
             Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid, Solid,
@@ -78,19 +88,19 @@ fn init_stage() -> Stage {
     ]
 }
 
-struct World {
+struct WorldScreen {
     screen_width: f32,
     screen_height: f32,
 }
 
 struct Player {
-    x: f32,
-    y: f32,
+    collider: Actor,
+    speed: Vec2,
 }
 
-impl World {
-    fn new() -> World {
-        World {
+impl WorldScreen {
+    fn new() -> WorldScreen {
+        WorldScreen {
             screen_width: screen_width(),
             screen_height: screen_height(),
         }
@@ -101,15 +111,16 @@ fn draw_stage(stage: &Stage) {
     for (y, line) in stage.iter().enumerate() {
         for (x, block) in line.iter().enumerate() {
             draw_rectangle(
-                (x as f32 * BLOCK_SIZE) as f32,
-                (y as f32 * BLOCK_SIZE) as f32,
+                x as f32 * BLOCK_SIZE,
+                y as f32 * BLOCK_SIZE,
                 BLOCK_SIZE,
                 BLOCK_SIZE,
                 {
                     match block {
-                        Block::Air => BLUE,
-                        Block::Solid => BROWN,
-                        Block::Debug => RED,
+                        Tile::Empty => BLUE,
+                        Tile::Solid => BROWN,
+                        Tile::JumpThrough => todo!(),
+                        Tile::Collider => todo!(),
                     }
                 },
             );
@@ -117,7 +128,7 @@ fn draw_stage(stage: &Stage) {
     }
 }
 
-fn draw_mire(world: &World, camera: &Camera2D) {
+fn draw_mire(world: &WorldScreen, camera: &Camera2D) {
     let middle_height = world.screen_height / 2.;
     let middle_width = world.screen_width / 2.;
 
@@ -145,6 +156,11 @@ fn draw_mire(world: &World, camera: &Camera2D) {
     );
 }
 
+fn draw_player(player: &Player, world: &World) {
+    let pos = world.actor_pos(player.collider);
+    draw_rectangle_lines(pos.x, pos.y, PLAYER_WIDTH, PLAYER_HEIGHT, 1., GREEN)
+}
+
 #[macroquad::main("Duel")]
 async fn main() {
     let stage = init_stage();
@@ -155,12 +171,51 @@ async fn main() {
         BLOCK_SIZE * stage.len() as f32,
     ));
     set_camera(&camera);
+    let mut world = World::new();
+
+    let mut player = Player {
+        collider: world.add_actor(vec2(50.0, 80.0), 8, 8),
+        speed: vec2(0., 0.),
+    };
     loop {
         clear_background(LIGHTGRAY);
-        let world = World::new();
+        let world_screen = WorldScreen::new();
 
+        world.add_static_tiled_layer(
+            stage.iter().cloned().flatten().collect(),
+            BLOCK_SIZE,
+            BLOCK_SIZE,
+            16,
+            1,
+        );
         draw_stage(&stage);
-        draw_mire(&world, &camera);
+        draw_mire(&world_screen, &camera);
+        draw_player(&player, &world);
+
+        // player movement control
+        {
+            let pos = world.actor_pos(player.collider);
+            let on_ground = world.collide_check(player.collider, pos + vec2(0., 1.));
+
+            if !on_ground {
+                player.speed.y += 500. * get_frame_time();
+            }
+
+            if is_key_down(KeyCode::Right) {
+                player.speed.x = 100.0;
+            } else if is_key_down(KeyCode::Left) {
+                player.speed.x = -100.0;
+            } else {
+                player.speed.x = 0.;
+            }
+
+            if is_key_pressed(KeyCode::Space) && on_ground {
+                player.speed.y = -120.;
+            }
+
+            world.move_h(player.collider, player.speed.x * get_frame_time());
+            world.move_v(player.collider, player.speed.y * get_frame_time());
+        }
         next_frame().await
     }
 }
